841e108ed06032e788fb39f22823149db5a2fc43
John Davenport <jdavenport@fortinet.com>
2022-04-12 15:23:06 -0700

    add some test data

--- src/logreview/unpack.py
+++ src/logreview/unpack.py
@@ -12,10 +12,14 @@ def run_command(raw_command):
 
 
 def prepare_target_path(target_path, customer_number):
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
+        
     customer_path=os.path.join(target_path, customer_number)
     try:
         os.mkdir(customer_path)
     except FileExistsError:
+        #if the file already exists create a new folder using the date and time string
         date_string=strftime('%Y%m%d_%H%M%S')
         customer_path=os.path.join(customer_path, date_string)
         os.mkdir(customer_path)
@@ -41,18 +45,11 @@ def unpack(root_path, target_path, customer_number, sample_tar):
 
 
 
+if __name__== '__main__':
 
-
-root_path=r'/Users/jdavenport/Downloads'
-target_path=r'/Users/jdavenport/Downloads/tempLogs'
-customer_number='5900299'
-tar_filename='collector-AOLogs-3.tar'
-unpack(root_path, target_path, customer_number, tar_filename)
-                
-
-
-
-
-
-
-print('done')
+    root_path=r'/Users/jdavenport/Projects/LogReview/tests/data'
+    target_path=r'/Users/jdavenport/Projects/LogReview/tests/data/tempLogs'
+    customer_number='1100299'
+    tar_filename='AOLogs.tar'
+    unpack(root_path, target_path, customer_number, tar_filename)
+    print('done')
