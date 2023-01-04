16029ca581d72a0daa831bacd2d62f885bfadb06
John Davenport <jdavenport@fortinet.com>
2023-01-04 15:01:11 -0800

    handle backend phoenix.log better

--- src/logreview/load.py
+++ src/logreview/load.py
@@ -5,12 +5,28 @@ import pytz
 from datetime import datetime
 
 
-def get_sheets(path, filename, sheetname):
+def get_excel_sheet(path, filename, sheetname):
     excel_file = pd.read_excel(os.path.join(path, filename), sheet_name=sheetname)
     return excel_file
 
 
-def get_logs(path, filename):
+def verify_backend_phoenix_date(date_string):
+    """date should be in format:
+    2022-11-29T01:15:31.976088-08:00
+    """
+    try:
+        valid_date = re.match(
+            r"^(\d{4})[-](\d{2})[-](\d{2})[T](\d{2})[:](\d{2})[:](\d{2})[.](\d{6})[-](\d{2})[:](\d{2})",
+            date_string,
+        )
+        if valid_date is not None:
+            return True
+    except ValueError as e:
+        print(f"Invalid Date: {date_string}")
+    return False
+
+
+def get_backend_phoenix_log(path, filename):
     log_file = os.path.join(path, filename)
     df = pd.DataFrame(
         columns=[
@@ -19,9 +35,6 @@ def get_logs(path, filename):
             "pid",
             "error_code",
             "severity",
-            "process",
-            "file",
-            "line",
             "detail",
         ]
     )
@@ -30,18 +43,19 @@ def get_logs(path, filename):
         for line in f:
 
             spaces = line.split(" ")
-            remaining = " ".join(spaces[3:]).strip("\n").partition(":")
-            final = remaining[2].split(",", 4)
-
             date = spaces[0]
             hostname = spaces[1]
             pid = spaces[2].replace(":", "")
+
+            remaining = " ".join(spaces[3:]).strip("\n").partition(":")
             error_code = remaining[0].strip("[]")
-            severity = final[0].partition("=")[2]
-            process = final[1].partition("=")[2]
-            file_name = final[2].partition("=")[2]
-            line = final[3].partition("=")[2]
-            detail = final[4].partition("=")[2]
+            if error_code == error_code.upper():
+                severity = remaining[2].split(",", 4)[0].partition("=")[2]
+                detail = remaining[1:]
+            else:
+                error_code = "NONE"
+                severity = "NONE"
+                detail = remaining
 
             df = df.append(
                 {
@@ -50,9 +64,6 @@ def get_logs(path, filename):
                     "pid": pid,
                     "error_code": error_code,
                     "severity": severity,
-                    "process": process,
-                    "file": file_name,
-                    "line": line,
                     "detail": detail,
                 },
                 ignore_index=True,
@@ -72,7 +83,7 @@ def parse_datetime(x):
     return dt.replace(tzinfo=pytz.FixedOffset(dt_tz))
 
 
-def get_ssl_access_logs(path, filename):
+def get_ssl_access_log(path, filename):
     data = pd.read_csv(
         os.path.join(path, filename),
         sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
--- tests/test_load.py
+++ tests/test_load.py
@@ -10,13 +10,13 @@ log_file = "mAOLogs/AOLogs/backend/phoenix.log"
 ssl_access_file = "mAOLogs/AOLogs/system/var_log_httpd_log/ssl_access_log"
 
 
-def test_get_sheets_returns_DataFrame_object():
+def test_get_excel_sheet_returns_DataFrame_object():
     sheetname = "backend"
-    result = load.get_sheets(folder, excel_file, sheetname)
+    result = load.get_excel_sheet(folder, excel_file, sheetname)
     assert isinstance(result, DataFrame)
 
 
-def test_get_sheets_uses_row0_as_headers():
+def test_get_excel_sheet_uses_row0_as_headers():
     sheetname = "backend"
     list_of_columns = [
         "Error Code",
@@ -26,18 +26,23 @@ def test_get_sheets_uses_row0_as_headers():
         "How to Fix/KB",
         "Associated Tickets",
     ]
-    result = load.get_sheets(folder, excel_file, sheetname)
+    result = load.get_excel_sheet(folder, excel_file, sheetname)
     assert len(result.columns) == len(list_of_columns)
     assert all(a == b for a, b in zip(result.columns, list_of_columns))
 
 
-def test_get_logs_returns_DataFrame_object():
-    result = load.get_logs(folder, log_file)
+def test_get_backend_phoenix_log_returns_DataFrame_object():
+    result = load.get_backend_phoenix_log(folder, log_file)
     assert isinstance(result, DataFrame)
 
 
-def test_get_ssl_access_logs_returns_DataFrame_object():
-    result = load.get_ssl_access_logs(folder, ssl_access_file)
+def test_get_backend_phoenix_log_date_first_column():
+    result = load.get_backend_phoenix_log(folder, log_file)
+    assert load.verify_backend_phoenix_date(result.date[0])
+
+
+def test_get_ssl_access_log_returns_DataFrame_object():
+    result = load.get_ssl_access_log(folder, ssl_access_file)
     assert isinstance(result, DataFrame)
 
 
@@ -59,3 +64,8 @@ def test_parse_datetime_returns_datetime():
 def test_parse_datetime_parses_dates():
     result = load.parse_datetime("[12/Apr/2022:01:00:02 -0700]")
     assert result == datetime(2022, 4, 2, 1, 0, tzinfo=pytz.FixedOffset(70))
+
+
+def test_verify_backend_phoenix_date_fails_wrong_format():
+    date_string = "12/Apr/2022:01:00:02 -0700"
+    assert not load.verify_backend_phoenix_date(date_string)
